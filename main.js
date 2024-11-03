67fa910ea32e1e74f8d1d087d8e4648ed85a9c04
Daniel Xu <danielxu12217@gmail.com>
2024-11-03 16:58:51 +0800

    created overall grid layout for mercedes website without  js

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    lt
+    // navbar animitions
+
+    // for when scrollbar is all the way at the top 
+    
+    
+    // for when scrollbr is in the middle
+
+
+});
